cc399139041eabd2370e0615a28a7c8bccd6418f
Rohan <deepakrohan@hotmail.com>
2015-05-20 03:50:19 -0400

    Added a object and fetching the object to the front end

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 
+(function () {
 
 var app = angular.module("MyApp", []);
+	var gem = {
+		name: "Emrald",
+		price: "2.95",
+		description: "This is a Red Stone"
+	}
+
+app.controller('StoreController',  function(){
+	this.product = gem;
+});
+})();

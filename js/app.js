0d1edc70851ce45efb55039b5250f98d8d4fa528
Rohan <deepakrohan@hotmail.com>
2015-05-19 21:56:13 -0400

    Stable as per the codeSchool

--- js/app.js
+++ js/app.js
@@ -1,20 +1,3 @@
-(function () {
-	var myApp = angular.module('myApp',[]);
 
-	var GreetingController = function ($scope, $http) {
-		var onUserComplete = function  (response) {
-			$scope.user = response.data;
-		};
 
-		var onNotFound = function (reason) {
-			// alert(reason.message);
-			$scope.error = "Error Unable to Fetch Data";
-		};
-
-		$http.get("https://api.github.com/users/deepakrohan1").then(onUserComplete,onNotFound);
-		//then is a function which is followed by success , failure functions
-		// $scope.greeting = 'Hi angular';
-	};
-
-	myApp.controller('GreetingController',["$scope", "$http", GreetingController]);
-}());
+var app = angular.module("MyApp", []);

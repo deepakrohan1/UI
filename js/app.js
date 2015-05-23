523e069efbc76cd5a13a018cb8e088c4392af5ff
Rohan <deepakrohan@hotmail.com>
2015-05-23 01:00:28 -0400

    Learning Bootstrap

--- js/app.js
+++ js/app.js
@@ -1,14 +1,38 @@
 
 (function () {
 
-var app = angular.module("MyApp", []);
-	var gem = {
+	var app = angular.module("MyApp", []);
+	var gems = [
+	{
 		name: "Emrald",
-		price: "2.95",
-		description: "This is a Red Stone"
+		price: 2.95,
+		description: "This is a Red Stone",
+		soldOut: true,
+		images: {
+			image1:"images/emerald.jpg"
+		}
+	},
+	{
+		name: "Ruby",
+		price: 5.53,
+		description: "This is a red stone used in making jwellery",
+		soldOut: false,
+		images: {
+			image1:"images/ruby.jpg"
+		}
+	},
+	{
+		name: "Sapphire",
+		price: 1,
+		description: "Harder gem stone when compared to the other types",
+		soldOut: true,
+		images: {
+			image1:"images/sapphire.jpg"
+		}
 	}
+	]
 
-app.controller('StoreController',  function(){
-	this.product = gem;
-});
+	app.controller('StoreController',  function(){
+		this.products = gems;
+	});
 })();
